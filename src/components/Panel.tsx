62cefcdc0a43a95720e07324f15db611bc9999f1
NahuelTG <nahueltorrezg@gmail.com>
2026-01-04 14:52:56 -0400

    add: implement Pronostics component and integrate it into Panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,9 +1,11 @@
 import { SearchCity } from "./SearchCity";
+import { Pronostics } from "./Pronostics";
 
 export const Panel = () => {
    return (
       <div>
          <SearchCity />
+         <Pronostics />
       </div>
    );
 };
---
+++ src/components/Pronostics.tsx
@@ -0,0 +1,46 @@
+export const Pronostics = () => {
+   return (
+      <>
+         <section>
+            <header>
+               <h2>Pronóstico 7 días</h2>
+            </header>
+            <div>
+               <article>
+                  <h4>Hoy</h4>
+                  //SVG tipo de nube
+                  <p>Chubascos</p>
+                  <div>
+                     <p>26°</p>
+                     <p>Max</p>
+                     <p>13°</p>
+                     <p>Min</p>
+                  </div>
+               </article>
+               <article>
+                  <h4>Hoy</h4>
+                  //SVG tipo de nube
+                  <p>Chubascos</p>
+                  <div>
+                     <p>26°</p>
+                     <p>Max</p>
+                     <p>13°</p>
+                     <p>Min</p>
+                  </div>
+               </article>
+               <article>
+                  <h4>Hoy</h4>
+                  //SVG tipo de nube
+                  <p>Chubascos</p>
+                  <div>
+                     <p>26°</p>
+                     <p>Max</p>
+                     <p>13°</p>
+                     <p>Min</p>
+                  </div>
+               </article>
+            </div>
+         </section>
+      </>
+   );
+};
